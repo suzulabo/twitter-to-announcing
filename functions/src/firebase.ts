215232153ad035e47a1ac3f4b546d6a4a5c225bc
KJ <41730006+suzulabo@users.noreply.github.com>
2021-09-23 03:05:32 +0900

    add posts

--- functions/src/firebase.ts
+++ functions/src/firebase.ts
@@ -1,4 +1,7 @@
 import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
 
 export type FirebaseAdminApp = admin.app.App;
 export type Firestore = admin.firestore.Firestore;
+export type HttpRequest = functions.Request;
+export type HttpResponse = functions.Response;
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,31 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import { fetchTweets } from './fetch';
+import { httpsGetPosts } from './posts';
 
 const adminApp = admin.initializeApp();
 adminApp.firestore().settings({ ignoreUndefinedProperties: true });
 
-export const onPubSubFetchTweets = functions.pubsub.schedule('*/5+1 * * * *').onRun(async () => {
-  await fetchTweets(adminApp);
-  return 0;
-});
+export const posts = functions
+  .runWith({
+    memory: '128MB',
+    timeoutSeconds: 10,
+    minInstances: 0,
+    maxInstances: 1,
+  })
+  .https.onRequest(async (req, res) => {
+    await httpsGetPosts(req, res, adminApp);
+  });
+
+export const onPubSubFetchTweets = functions
+  .runWith({
+    memory: '128MB',
+    timeoutSeconds: 30,
+    minInstances: 0,
+    maxInstances: 1,
+  })
+  .pubsub.schedule('*/5+1 * * * *')
+  .onRun(async () => {
+    await fetchTweets(adminApp);
+    return 0;
+  });
