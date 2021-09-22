355c47727938b398a902779c93d469e593a95c9b
KJ <41730006+suzulabo@users.noreply.github.com>
2021-09-23 04:24:04 +0900

    deploy

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -24,7 +24,7 @@ export const onPubSubFetchTweets = functions
     minInstances: 0,
     maxInstances: 1,
   })
-  .pubsub.schedule('*/5+1 * * * *')
+  .pubsub.schedule('1,6,11,16,21,26,31,36,41,46,51,56 * * * *')
   .onRun(async () => {
     await fetchTweets(adminApp);
     return 0;
