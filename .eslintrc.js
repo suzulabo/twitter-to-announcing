259acdfeccbd1e1bbb1e8331a3b3fcc681bced5d
KJ <41730006+suzulabo@users.noreply.github.com>
2021-09-22 11:59:20 +0900

    init root

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  overrides: [
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      parserOptions: {
+        project: ['tsconfig.json'],
+      },
+      settings: {
+        react: {
+          version: '17.0.1',
+        },
+      },
+      extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+        '@typescript-eslint/no-floating-promises': 'error',
+
+        'require-await': 'off',
+        '@typescript-eslint/require-await': 'warn',
+      },
+    },
+  ],
+};
